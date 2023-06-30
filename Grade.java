1bb01f35b33eb9aa067f6808b483b0f1daa0cd60
Boncc <bakkamil7@gmail.com>
2023-06-30 11:20:20 +0200

    Added a congratulatory/failing message for the user

--- Grade.java
+++ Grade.java
@@ -4,13 +4,23 @@ public class Grade {
     public static void main(String[] args) {
 
         int points;
+        String name;
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Your Name: ");
+        name = input.nextLine();
+
         System.out.print("Enter Number of Points Awarded: ");
         points = input.nextInt();
 
         double grade = getGrade(points);
-        System.out.println("Your Grade Is: " + grade);
+        System.out.println(name + ", Your Grade Is: " + grade);
 
+        if (grade >= 3) {
+            System.out.println("Congratulations, " + name + "! You have passed.");
+        } else {
+            System.out.println("Sorry, " + name + ". You have failed. Better luck next time!");
+        }
 
     }
 
