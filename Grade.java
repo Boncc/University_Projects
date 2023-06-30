0531bca3f4074981679a1f558eb6f35d9760d0d9
Boncc <bakkamil7@gmail.com>
2023-06-30 04:07:06 +0200

    Added two source files

---
+++ Grade.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Grade {
+    public static void main(String[] args) {
+
+        int points;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number of Points Awarded: ");
+        points = input.nextInt();
+
+        double grade = getGrade(points);
+        System.out.println("Your Grade Is: " + grade);
+
+
+    }
+
+    public static double getGrade(int points) {
+        double grade;
+        if (points <= 50) {
+            grade = 2;
+        } else if (points <= 60) {
+            grade = 3;
+        } else if (points <= 70) {
+            grade = 3.5;
+        } else if (points <= 80) {
+            grade = 4;
+        } else if (points <= 90) {
+            grade = 4.5;
+        } else grade = 5;
+
+        return grade;
+    }
+}
---
+++ Rhombus.java
@@ -0,0 +1,39 @@
+public class Rhombus {
+    public static void main(String[] args) {
+        int x = 2; // width
+        int y = 4; // height
+
+        // Check if width is even
+        if (x % 2 == 0) {
+            x++; // increase it by one to make it odd (rhombus should have odd width)
+        }
+
+        // Determine the height of the upper and lower parts of the rhombus
+        int upperHeight = (int) Math.ceil(y / 2.0);
+        int lowerHeight = y - upperHeight;
+
+        // Print the upper part of the rhombus
+        for (int i = 0; i < upperHeight; i++) {
+            for (int j = 0; j < x; j++) {
+                if (j >= (x / 2) - i && j <= (x / 2) + i) {
+                    System.out.print("/");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        // Print the lower part of the rhombus
+        for (int i = lowerHeight - 1; i >= 0; i--) {
+            for (int j = 0; j < x; j++) {
+                if (j >= (x / 2) - i && j <= (x / 2) + i) {
+                    System.out.print("/");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
