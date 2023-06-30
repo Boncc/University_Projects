f6e654c3f645cada4fe9c7a5a37f6b269ba6c9a8
Boncc <bakkamil7@gmail.com>
2023-06-30 04:14:50 +0200

    Changed the slash symbols to dots

--- Rhombus.java
+++ Rhombus.java
@@ -1,7 +1,7 @@
 public class Rhombus {
     public static void main(String[] args) {
-        int x = 2; // width
-        int y = 4; // height
+        int x = 8; // width
+        int y = 10; // height
 
         // Check if width is even
         if (x % 2 == 0) {
@@ -16,7 +16,7 @@ public class Rhombus {
         for (int i = 0; i < upperHeight; i++) {
             for (int j = 0; j < x; j++) {
                 if (j >= (x / 2) - i && j <= (x / 2) + i) {
-                    System.out.print("/");
+                    System.out.print(".");
                 } else {
                     System.out.print(" ");
                 }
@@ -28,7 +28,7 @@ public class Rhombus {
         for (int i = lowerHeight - 1; i >= 0; i--) {
             for (int j = 0; j < x; j++) {
                 if (j >= (x / 2) - i && j <= (x / 2) + i) {
-                    System.out.print("/");
+                    System.out.print(".");
                 } else {
                     System.out.print(" ");
                 }
